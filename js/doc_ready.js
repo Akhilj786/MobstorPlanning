5fa6db2263cec9c851e53de35aae309351300c5d
Akhil Jain <akhilja@boxvisitmean-lm.corp.yahoo.com>
2013-08-25 04:35:27 -0700

    Split up of JS Files

--- js/doc_ready.js
+++ js/doc_ready.js
@@ -1,90 +1,109 @@
-	$.getScript("../js/formvalidate.js");
-	$(document).ready(function() {
 
-				var url = 'https://s1.yimg.com/rz/l/yahoo_en-US_f_p_135x24.png';
-				// URL of the image
-				$('<img>')// create a new image element
-				.attr('src', url)// set src
-				.appendTo("#yahoologoid");
+$.getScript("http://jquery.bassistance.de/validate/jquery.validate.js");
+$.getScript("http://jquery.bassistance.de/validate/additional-methods.js");
+$(document).ready(function() {
 
-				//Ajax Farmname autocomplete
-				$("#fname_req").autocomplete({
-					source : "get_farm.php",
-					minLength : 1,
-					select : function(evt, ui) {
+	var url = 'https://s1.yimg.com/rz/l/yahoo_en-US_f_p_135x24.png';
+	// URL of the image
+	$('<img>')// create a new image element
+	.attr('src', url)// set src
+	.appendTo("#yahoologoid");
 
-						var fname = ui.item.value;
-						var fsize = ui.item.size;
-						var frps = ui.item.rps;
-						var fwps = ui.item.wps;
-						this.form.fsize.value = fsize;
-						this.form.frps.value = frps;
-						this.form.fwps.value = fwps;
-						//edit Farm_benchmark 
-						$('#edit_id').click(function() {
-							jQuery.ajax({
-								type : "POST",
-								url : "editFarm.php",
-								data : {
-									fname : $('#fname_req').val(),
-									fsize : $('#fsize_req').val(),
-									frps : $('#frps_req').val(),
-									fwps : $('#fwps_req').val()
-								},
-								cache : false,
-								success : function(response) {
-									if (response) {
-										alert(response);
+	$('#addFarmbenchmarkid').click(function() {
+		//document.location.href = 'newFarmbenchmark.php';
+		$( "#dialog" ).dialog( "open" );
+		$('#biggercontainerid').css("opacity","0.4");
 
-									}
+	});
 
-								}
-							});
-						});
+	//Ajax Farmname autocomplete
+	$("#fname_req").autocomplete({
+		source : "get_farm.php",
+		minLength : 1,
+		select : function(evt, ui) {
 
-					}
-				});
-
-				$("#dcancel_id").click(function() {
-
-					document.forms["New_domain"].reset();
-					$("#FarmTableid").html("");
-					$("#sizeGraphid").html("");
-					$("#wpsGraphid").html("");
-
-				});
-
-				$('#Save_id').click(Table_fill);
-
-			});
-
-			function Table_fill() {
-				//Prediction table
-				
-				
-				if($('#size_req').val()!=null && $('#size_req').val()!==''){
-				var size = $('#size_req').val();
-				var wps = $('#wps_req').val();
+			var fname = ui.item.value;
+			var fsize = ui.item.size;
+			var frps = ui.item.rps;
+			var fwps = ui.item.wps;
+			this.form.fsize.value = fsize;
+			this.form.frps.value = frps;
+			this.form.fwps.value = fwps;
+			//edit Farm_benchmark
+			$('#edit_id').click(function() {
 				jQuery.ajax({
 					type : "POST",
-					url : "HoltWinter.php",
+					url : "editFarm.php",
 					data : {
-						size : size,
-						wps : wps
+						fname : $('#fname_req').val(),
+						fsize : $('#fsize_req').val(),
+						frps : $('#frps_req').val(),
+						fwps : $('#fwps_req').val()
 					},
 					cache : false,
 					success : function(response) {
 						if (response) {
-							$('#FarmTableid').show();
-							$('#Farm_prediction').show();
-							$('#Farm_prediction tbody').html("");
-							$('#Farm_prediction tbody').html(response);
+							alert(response);
 
 						}
 
 					}
 				});
-				}
-				else
-				alert("Fill Entire Form");
+			});
+
+		}
+	});
+
+	
+	
+
+
+	
+
+	$("#dialog").dialog({
+		autoOpen : false,
+
+		buttons : {
+			"Save" : function() {
+				savefunction();
+			},
+			Cancel : function() {
+				
+				$(this).dialog("close");
+				$('#biggercontainerid').css("opacity","1");
+				
 			}
+		},
+		close: function( event, ui ) {
+			$('#biggercontainerid').css("opacity","1");
+			}
+	});
+	
+function savefunction(){
+	var newfname=$('#newfname_req').val();
+	var newfsize=$('#newfsize_req').val();
+	var newfrps=$('#newfrps_req').val();
+	var newfwps=$('#newfwps_req').val();
+	jQuery.ajax({
+			type : "POST",
+			url : "newFarmSubmit.php",
+			data : {
+				newfname : newfname,
+				newfsize : newfsize,
+				newfrps	 : newfrps,
+				newfwps  : newfwps
+			},
+			cache : false,
+			success : function(response) {
+				alert(response);		
+		
+			}
+		});
+}
+
+
+
+
+
+});
+
